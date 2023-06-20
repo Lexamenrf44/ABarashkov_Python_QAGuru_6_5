5c5d6d911d981830d569c93de302257f7f600386
ABarashkov <alexander.stcstudent@gmail.com>
2023-06-20 15:46:27 +0400

    commentary fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,15 +4,8 @@ from selene.support.shared import config
 
 
 @pytest.fixture(autouse=True)
-def setup_teardown():
-    browser.open(config.base_url)
-    yield
-    browser.quit()
-
-
-@pytest.fixture(autouse=True)
 def setup_browser():
     config.timeout = 10
     config.window_width = 1680
     config.window_height = 1050
-    config.base_url = 'https://demoqa.com/automation-practice-form'
+    config.base_url = 'https://demoqa.com'
--- tests/test_demoqa_practice_form.py
+++ tests/test_demoqa_practice_form.py
@@ -5,6 +5,13 @@ import os
 
 def test_fill_check_practice_form():
 
+    # Should open page
+    browser.open('/automation-practice-form')
+
+    # Should perform tips and tricks tactic
+    browser.execute_script('document.querySelector("#fixedban").remove()')
+    browser.element('footer').execute_script('element.remove()')
+
     # Should fill elements
     browser.element('#firstName').type('Alex')
     browser.element('#lastName').type('Barashka')
@@ -24,19 +31,17 @@ def test_fill_check_practice_form():
     browser.element('#state #react-select-3-option-0').click()
     browser.element('#city').click()
     browser.element('#city #react-select-4-option-0').click()
-    browser.element('#submit').click()
+    browser.element('#submit').press_enter()
 
     # Should check elements
     browser.element(".modal-content").should(be.visible)
-    success_message = '.modal-header'
-    assert success_message == "Thanks for submitting the form"
-
+    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
     browser.all('.table-responsive .table tbody > tr')[0].should(have.text('Alex Barashka'))
     browser.all('.table-responsive .table tbody > tr')[1].should(have.text('abarashka@gmail.com'))
     browser.all('.table-responsive .table tbody > tr')[2].should(have.text('Male'))
     browser.all('.table-responsive .table tbody > tr')[3].should(have.text('1234567890'))
     browser.all('.table-responsive .table tbody > tr')[4].should(have.text('12 December,1996'))
-    browser.all('.table-responsive .table tbody > tr')[5].should(have.text('Physics'))
+    browser.all('.table-responsive .table tbody > tr')[5].should(have.text('English'))
     browser.all('.table-responsive .table tbody > tr')[6].should(have.text('Sports'))
     browser.all('.table-responsive .table tbody > tr')[7].should(have.text('chatgpt_logo.png'))
     browser.all('.table-responsive .table tbody > tr')[8].should(have.text('Tbilisi'))
